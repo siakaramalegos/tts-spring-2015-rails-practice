4a9bbd42d96938ab9de04970fc10b7b0906a3ecc
Sia <siakaramalegos@gmail.com>
2015-05-16 20:35:19 -0500

    Sorted some views alphabetically

--- pre_music_db/app/models/artist.rb
+++ pre_music_db/app/models/artist.rb
@@ -4,4 +4,10 @@ class Artist < ActiveRecord::Base
 
   validates :name, presence: true, length: {minimum: 2}, uniqueness: true
   validates :genre_id, presence: true
+
+  # Creating a recent scope that gives items created within the last indicated minutes.
+  # The lamda will freshly run every time the scope is called.
+  # Must exit and re-enter Rails console each time this file is updated.
+  scope :recent, -> (minutes_past=60) {where("created_at > ?", minutes_past.minutes.ago)}
+  scope :today, -> { where('DATE(created_at) = ?', Date.today)}
 end
